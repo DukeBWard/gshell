a3c1290610f0dab538b18b073283df03c4b02e26
Luke Ward <wardlukeb@gmail.com>
2024-06-11 10:22:54 -0400

    added basic prompt loop

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gshell
+
+go 1.22.3
--- main.go
+++ main.go
@@ -4,22 +4,31 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Cannot use := at the package level
 
 func main() {
 	exit := false
-	cusor := bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout))
-
-	input, err := cusor.ReadString('\n')
-	if err != nil {
-		fmt.Println("Error with input:", err)
-		return
-	}
+	cursor := bufio.NewReadWriter(bufio.NewReader(os.Stdin), bufio.NewWriter(os.Stdout))
 
 	for !exit {
+		cursor.WriteString("gshell> ")
+		// WriteString writes to a buffer so you need to flush it to display it
+		cursor.Flush()
+
+		input, err := cursor.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error with input:", err)
+			return
+		}
+
+		input = strings.TrimSpace(input)
+
+		if input == "exit" {
+			exit = true
+		}
 
 	}
-
 }
