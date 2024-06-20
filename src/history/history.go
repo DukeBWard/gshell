ea0bfe2ac7a381b2a80d8665aba4afd11e23a25d
Luke Ward <wardlukeb@gmail.com>
2024-06-20 10:24:11 -0400

    added more history modifications

--- src/history/history.go
+++ src/history/history.go
@@ -7,14 +7,38 @@ import (
 
 const HIST_NAME = ".gshell_history"
 
-func Append_history(cmd string) {
+func Append_history(cmd string) error {
 
-	data := []byte(cmd)
+	// check for file
+	_, err := os.Stat(HIST_NAME)
+	if os.IsNotExist(err) {
+		file, err := os.Create(HIST_NAME)
+		if err != nil {
+			return fmt.Errorf("cannot create file: %w", err)
 
-	err := os.WriteFile(HIST_NAME, data, 0777)
-	if err != nil {
-		fmt.Println(err)
+		}
+		defer file.Close()
 	}
+
+	file, err := os.OpenFile(HIST_NAME, os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("cannot open file: %w", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(cmd)
+	if err != nil {
+		return fmt.Errorf("cannot write to file: %w", err)
+	}
+
+	return nil
+
+	// data := []byte(cmd)
+
+	// err := os.WriteFile(HIST_NAME, data, 0777)
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
 
 	// data, err = os.ReadFile(HIST_NAME)
 	// if err != nil {
